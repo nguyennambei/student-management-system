fcefa6312c9fed885f299bfe8762acd7d9337c88
namu <yxmedia181004@gmail.com>
2019-11-18 14:34:59 +0900

    add input

--- src/components/studentData/AddStudent.js
+++ src/components/studentData/AddStudent.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function AddStudent(){
     return(
-        <p>Add Student</p>
+        <div>
+        <h3>学生情報を追加</h3>
+        <div>
+            <input></input>
+        </div>
+        </div>
     );
 }
